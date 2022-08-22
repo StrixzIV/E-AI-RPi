71fa010e6d2976f0b761981e9d3d386a60ee213e
Jirayu <jirayukaewsing48@gmail.com>
2022-08-22 07:20:09 +0700

    Added relay assignment

---
+++ relay.py
@@ -0,0 +1,33 @@
+import time
+from RPi import GPIO as gpio
+
+btns = [19, 26]
+relays = [20, 21]
+
+gpio.setmode(gpio.BCM)
+gpio.setup(21, gpio.OUT)
+
+for btn in btns:
+    gpio.setup(btn, gpio.IN, pull_up_down = gpio.PUD_UP)
+
+for relay in relays:
+    gpio.setup(relay, gpio.OUT)
+
+while True:
+
+    state1 = not (True if gpio.input(btns[0]) == 1 else False)
+    state2 = not (True if gpio.input(btns[1]) == 1 else False)
+
+    if state1:
+        gpio.output(relays[0], False)
+        gpio.output(relays[1], True)
+
+    elif state2:
+        gpio.output(relays[0], True)
+        gpio.output(relays[1], False)
+
+    else:
+        gpio.output(relays[0], False)
+        gpio.output(relays[1], False)
+
+    time.sleep(0.5)
