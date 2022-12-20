2ea91ba320c9d76004812708f333dfd1592d97fc
Jirayu <jirayukaewsing48@gmail.com>
2022-12-20 16:51:43 +0700

    Add color detection

--- Semester-2/conveyer-belt/main.py
+++ Semester-2/conveyer-belt/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import tkinter as tk
 import RPi.GPIO as gpio
 
@@ -12,23 +13,84 @@ in1 = 13
 in2 = 19
 control = 25
 
+yellow_count = 0
+red_count = 0
+
 wcam, hcam = 640, 480
 cap = cv2.VideoCapture(0)
 cap.set(3, wcam)
 cap.set(4, hcam)
 
-status = tk.StringVar()
+def yellow_detector(frame: np.ndarray[np.uint8], show_frame: np.ndarray[np.uint8]) -> np.ndarray[np.uint8]:
+    
+    global yellow_count
+    
+    lower_yellow = np.array((15, 150, 20))
+    upper_yellow = np.array((35, 255, 255))
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    mask = cv2.inRange(frame, lower_yellow, upper_yellow)
+    
+    (contours, hierachy) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if len(contours) != 0:
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 500:
+                
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                
+                if x in range(145, 200):
+                    yellow_count += 1
+                    
+                cv2.rectangle(show_frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    
+    return cv2.cvtColor(show_frame, cv2.COLOR_BGR2RGB)
+
+
+def red_detector(frame: np.ndarray[np.uint8], show_frame: np.ndarray[np.uint8]) -> np.ndarray[np.uint8]:
+    
+    global red_count
+    
+    lower_red = np.array((0, 87, 100))
+    upper_red = np.array((10, 255, 255))
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    mask = cv2.inRange(frame, lower_red, upper_red)
+    
+    (contours, hierachy) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if len(contours) != 0:
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 500:
+                
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                print(x)
+                
+                if x in range(145, 200):
+                    red_count += 1
+                    
+                cv2.rectangle(show_frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    
+    return cv2.cvtColor(show_frame, cv2.COLOR_BGR2RGB)
+
 
 def show_frame():
 
     global imgtk
     global frame
+    global status
 
     check, frame = cap.read()
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+    filtered = yellow_detector(frame, frame)
+    filtered = red_detector(frame, frame)
+    
+    status.set(f'Yellow: {yellow_count}\nRed: {red_count}')
     
     # convert image for GUI
-    img = Image.fromarray(cv2image)
+    img = Image.fromarray(filtered)
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
@@ -51,6 +113,8 @@ def belt_stop():
 
 
 window = tk.Tk() 
+
+status = tk.StringVar()
 
 window.wm_title("Conveyer belt controller")
 window.geometry('800x1000')
